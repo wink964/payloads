de15f213deaf3675fb51928cf89d6560c531efce
wink964 <wink_964@outlook.com>
2024-07-03 12:27:36 -0700

    test

---
+++ calc.ps1
@@ -0,0 +1,1 @@
+Start-Process -FilePath "calc.exe" -ArgumentList "/n"; Start-Process -FilePath "calc.exe" -ArgumentList "/n"; Start-Process -FilePath "calc.exe" -ArgumentList "/n"; Start-Process -FilePath "calc.exe" -ArgumentList "/n"
